4c6bc374ef253fa8009d7202d9ac5a5c72edce54
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-08-21 00:54:10 +0530

    Added Dockerfile

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into todo(done, id, target_date, description, username)
-values(false,1001,CURRENT_DATE() ,'Learn to Dance','saksham');
-
-insert into todo(done, id, target_date, description, username)
-values(false,1002,CURRENT_DATE() ,'Learn to Walk','saksham');
-
-insert into todo(done, id, target_date, description, username)
-values(false,1003,CURRENT_DATE() ,'Learn to Code Backend','saksham');
